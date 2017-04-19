c33b3ba5639681eebe23c2af366b37c69ce90082
hyposulphite <zhoutystorm@gmail.com>
2017-04-18 22:51:18 -0400

    update chapter3

--- tidytext/Chapter3.R
+++ tidytext/Chapter3.R
@@ -2,7 +2,12 @@ library(dplyr)
 library(janeaustenr)
 library(tidytext)
 library(ggplot2)
+library(gutenbergr)
 
+###3.1 Term frequency in Jane Austen’s novels
+
+#?# create book_words from austen_books: book, word, n=word count
+#?#   , total=word count by book. Sort by count
 book_words <- austen_books() %>%
   unnest_tokens(word, text) %>%
   count(book, word, sort = TRUE) %>%
@@ -17,14 +22,17 @@ book_words <- left_join(book_words, total_words)
 book_words
 
 
-
+#?# plot word frequency distribution, by book
 ggplot(book_words, aes(n/total, fill = book)) +
   geom_histogram(show.legend = FALSE) +
   xlim(NA, 0.0009) +
   facet_wrap(~book, ncol = 2, scales = "free_y")
 
 
-###4.2 Zipf's law
+###3.2 Zipf's law
+
+#?# create freq_by_rank: rank=count rank by book, term frequency
+#?#   plot `term frequency` by rank, overlay by book
 freq_by_rank <- book_words %>% 
   group_by(book) %>% 
   mutate(rank = row_number(), 
@@ -51,17 +59,20 @@ freq_by_rank %>%
   scale_x_log10() +
   scale_y_log10()
 
-###4.3 The bind_tf_idf function
 
-book_words <- book_words %>%
+###3.3 The bind_tf_idf function
+
+#?# create book_words1: get bind_tf_idf, sort by tf_idf
+book_words1 <- book_words %>%
   bind_tf_idf(word, book, n)
-book_words
+book_words1
 
-book_words %>%
+book_words1 %>%
   select(-total) %>%
   arrange(desc(tf_idf))
 
-plot_austen <- book_words %>%
+#?# plot tf_idf
+plot_austen <- book_words1 %>%
   arrange(desc(tf_idf)) %>%
   mutate(word = factor(word, levels = rev(unique(word))))
 
@@ -70,6 +81,7 @@ ggplot(plot_austen[1:20,], aes(word, tf_idf, fill = book)) +
   labs(x = NULL, y = "tf-idf") +
   coord_flip()
 
+#?# plot tf_idf by book
 plot_austen <- plot_austen %>% 
   group_by(book) %>% 
   top_n(15) %>% 
@@ -81,11 +93,14 @@ ggplot(plot_austen, aes(word, tf_idf, fill = book)) +
   facet_wrap(~book, ncol = 2, scales = "free") +
   coord_flip()
 
-###4.4 A corpus of physics texts
 
-library(gutenbergr)
+
+###3.4 A corpus of physics texts
+
+
+#?# create physics_word: gutenberg(37729, 14725, 13476, 5001), in word and count, add author
+#?#   1. plot tf_idf by author
 physics <- gutenberg_download(c(37729, 14725, 13476, 5001), meta_fields = "author")
-
 
 physics_words <- physics %>%
   unnest_tokens(word, text) %>%
