b9eeefb4ffb5370d81941ab3b07c63c7b4da877e
hyposulphite <zhoutystorm@gmail.com>
2017-05-03 23:11:58 -0400

    finish chapter3

--- tidytext/Exercise/Ex_Chapter3.R
+++ tidytext/Exercise/Ex_Chapter3.R
@@ -10,7 +10,8 @@ library(gutenbergr)
 #?#   , total=word count by book. Sort by count
 book_words = austen_books() %>% 
   unnest_tokens(word, text, token="words") %>% 
-  count(book, word, sort=TRUE)
+  count(book, word, sort=TRUE) %>% 
+  ungroup()
 
 total_words = book_words %>% 
   group_by(book) %>% 
@@ -50,15 +51,57 @@ freq_by_rank %>%
   scale_y_log10()
 
 ###3.3 The bind_tf_idf function
-
 #?# create book_words1: get bind_tf_idf, sort by tf_idf
+book_words1 = book_words %>% 
+  bind_tf_idf(n_col = n, term_col=word, document_col=book) %>% 
+  arrange(-tf_idf)
 
 #?# plot tf_idf
+book_words1 %>% 
+  top_n(n=20, wt=tf_idf) %>% 
+  mutate(word=reorder(word, tf_idf)) %>% 
+  ggplot(aes(x=word, y=tf_idf, fill=book)) +
+  geom_bar(stat="identity") +
+  coord_flip()
 
 #?# plot tf_idf by book
+book_words1 %>% 
+  group_by(book) %>% 
+  top_n(n=10, wt=tf_idf) %>% 
+  ungroup() %>% 
+  mutate(word=reorder(word, tf_idf)) %>% 
+  ggplot(aes(x=word, y=tf_idf, fill=book)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~book, ncol=2, scale="free") +
+  coord_flip()
+
 
 ###3.4 A corpus of physics texts
-
 #?# create physics_word: gutenberg(37729, 14725, 13476, 5001), in word and count, add author
-
 #?#   1. plot tf_idf by author
+
+physics_word0 = gutenberg_download(gutenberg_id = c(37729, 14725, 13476, 5001), meta_fields = "author")
+physics_word = physics_word0 %>% 
+  unnest_tokens(word, text, token="words") %>% 
+  count(word, author, sort=TRUE) %>% 
+  ungroup()
+
+my_stop_words=data_frame(word=c("k1","eq","ac","rc","cm","cg","cb","ak","bn"))
+
+physics_word %>% 
+  anti_join(stop_words) %>% 
+  anti_join(my_stop_words) %>% 
+  bind_tf_idf(word, author, n) %>% 
+  group_by(author) %>% 
+  top_n(15) %>% 
+  mutate(word=reorder(word, tf_idf)) %>% 
+  ggplot(aes(x=word, y=tf_idf, fill=author)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~author, scale="free") +
+  coord_flip()
+
+
+
+
+
+
